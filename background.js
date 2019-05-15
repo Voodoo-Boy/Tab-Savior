cf495b32a942a2c088a422d4594553f06b802706
Han Wang <davidwang10010@gmail.com>
2019-05-16 01:34:05 +0800

    Making some progress

--- background.js
+++ background.js
@@ -1,10 +1,48 @@
 'use strict'
 
+// Create "copy link text" item in context menu
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({In: '#3aa757'}, function() {
-        console.log("The color is green.");
+    console.log("Installing...");
+    chrome.contextMenus.create({
+        "id": "tabsavior-contextmenu-id",
+        "title": "copy link text",
+        "contexts": ["link"]
       });
-      alert("Tab savior installed");
 })
 
-// Create menu item for "Link" context type.
+// Register listener for the context menu item..
+chrome.contextMenus.onClicked.addListener(function() {
+    console.log("context menu clicked");
+
+    chrome.tabs.query({currentWindow: true}, function(tabs) {
+        // Extract the favIcon, title, url of tabs in current window.
+        var tabToSave = [];
+        tabs.forEach(tab => {
+            tabToSave.push({favIconUrl: tab.favIconUrl, title: tab.title, url: tab.url})
+        });
+
+        // Save to storage.
+        var jsonString = JSON.stringify(tabToSave, null, '\t');
+        chrome.storage.sync.set({tabs_1: tabToSave}, function() {
+            console.log("Saved");
+        });
+
+        // chrome.storage.sync.get("tabs_1", function(items) {
+        //     console.log("Get: ", items);
+        // });
+
+        chrome.storage.sync.getBytesInUse(null, function(bytesInUse) {
+            console.log("Bytes in use: ", bytesInUse);
+        });
+    });
+});
+
+chrome.runtime.onStartup.addListener(function() {
+    console.log("Starting...");
+    chrome.browserAction.setBadgeText({text: ""});
+});
+
+chrome.runtime.onSuspend.addListener(function() {
+    console.log("Unloading...");
+    chrome.browserAction.setBadgeText({text: ""});
+});
