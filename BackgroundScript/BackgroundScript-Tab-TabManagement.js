e9a27c504dec7f5eaa9852375afbf7a10ca4c95b
whan <davidwang10010@gmail.com>
2019-10-11 11:27:45 +0800

    Adjust code structure

---
+++ BackgroundScript/BackgroundScript-Tab-TabManagement.js
@@ -0,0 +1,18 @@
+// chrome.runtime.onInstalled.addListener(function () {
+//     console.log("Tab Savior Installed - Tab - Tab Management");
+//     chrome.contextMenus.create({
+//         "id": "CopyLinkName",
+//         "title": "Copy Link Text",
+//         "contexts": ["link"]
+//     }, function () {
+//         console.log("Context Menu for copy link name created")
+//     }
+//     );
+// });
+
+// chrome.contextMenus.onClicked.addListener(function (info, tab) {
+//     // When the copy link text context menu is clicked, execute a injected code snippet to get the link text.
+//     chrome.tabs.executeScript({
+//         code: 'navigator.clipboard.writeText(document.activeElement.innerText);'
+//     });
+// });
